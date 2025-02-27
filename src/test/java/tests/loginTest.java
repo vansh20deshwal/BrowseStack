c00a1503bada84c1963fcf861e48c3c3419bf73d
AnshuKeshri <anshukeshri1232003@gmail.com>
2025-02-27 10:22:02 +0530

    updated

--- src/test/java/tests/loginTest.java
+++ src/test/java/tests/loginTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import Base.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import Pages.Dashboard;
 import Pages.login;
@@ -9,19 +10,20 @@ import Pages.login;
 public class loginTest extends BaseTest {
 
     @Test
-    public void testValidLogin() {
+    @Parameters({"username", "password"})
+    public void testValidLogin(String username, String password) {
         driver.get("https://www.browserstack.com/users/sign_in");
 
-        // Initialize page objects
+       
         login loginPage = new login(driver);
         Dashboard dashboardPage = new Dashboard(driver);
 
-        // Perform login
-        loginPage.enterUsername("vansh@gmail.com");
-        loginPage.enterPassword("1234");
+       
+        loginPage.enterUsername(username);
+        loginPage.enterPassword(password);
         loginPage.clickLogin();
 
-        // Validate successful login
+       
         Assert.assertTrue(dashboardPage.getWelcomeMessage().contains("Welcome"));
     }
 }
